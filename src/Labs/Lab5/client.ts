53982806a332cdc7cacd645a2fb56eb4492ca7e7
Ritika Dhall <ritikapdhall@gmail.com>
2025-04-03 01:05:02 -0400

    bug fix for netlify

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -4,6 +4,7 @@ const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 
 export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
+  console.log(REMOTE_SERVER);
   return response.data;
 };
 
