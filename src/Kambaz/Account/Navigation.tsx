ed87033054a4bbe85ab5b15ef593ec25ed9e48fe
Ritika Dhall <ritikapdhall@gmail.com>
2025-04-06 19:45:54 -0400

    a6

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -6,15 +6,22 @@ export default function AccountNavigation() {
     const { currentUser } = useSelector((state: any) => state.accountReducer);
     const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
     const pathname = useLocation().pathname;
+    const active = (path: string) => (pathname.includes(path) ? "active" : "text-danger");
 
     return (
         <ListGroup id="wd-account-navigation" className="wd fs-5 rounded-0">
-            
+
             {links.map((link) => (
-                <ListGroup.Item key={link} to={`/Kambaz/Account/${link}`} as={Link} id={`wd-account-${link.toLowerCase()}-link`} className={`border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>
+                <ListGroup.Item key={link} to={`/Kambaz/Account/${link}`} as={Link} id={`wd-account-${link.toLowerCase()}-link`} className={`border border-0 ${active(link)}`}>
                     {link}
                 </ListGroup.Item>
             ))}
+
+            {currentUser && currentUser.role === "ADMIN" && (
+                <ListGroup.Item key={"Users"} to={`/Kambaz/Account/Users`} as={Link} className={`border border-0 ${active("Users")}`}>
+                    Users
+                </ListGroup.Item>
+            )}
 
         </ListGroup>
     );
--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import Signin from "./Signin";
 import Profile from "./Profile";
 import Signup from "./Signup";
+import Users from "./Users";
 import AccountNavigation from "./Navigation";
 
 export default function Account() {
@@ -17,10 +18,12 @@ export default function Account() {
                     </td>
                     <td valign="top">
                         <Routes>
-                            <Route path="/" element={<Navigate to={ currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin" } />} />
+                            <Route path="/" element={<Navigate to={currentUser ? "/Kambaz/Account/Profile" : "/Kambaz/Account/Signin"} />} />
                             <Route path="/Signin" element={<Signin />} />
                             <Route path="/Profile" element={<Profile />} />
                             <Route path="/Signup" element={<Signup />} />
+                            <Route path="/Users" element={<Users />} />
+                            <Route path="/Users/:uid" element={<Users />} />
                         </Routes>
                     </td>
                 </tr>
